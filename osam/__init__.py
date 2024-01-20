bcf66bf022655aa339b53662570df7b263652753
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-01-20 20:01:31 +0900

    Remove dependency on loguru

--- osam/__init__.py
+++ osam/__init__.py
@@ -1,3 +1,16 @@
 import importlib.metadata
+import logging
 
 __version__ = importlib.metadata.version("osam")
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(module)s:%(funcName)s:%(lineno)s - %(message)s"
+    )
+)
+logger.addHandler(handler)
