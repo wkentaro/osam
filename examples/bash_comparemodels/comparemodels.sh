8eacf0d78e5f19063202158abe63590191391521
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-01-21 02:17:57 +0900

    Add bash_comparemodels example

---
+++ examples/bash_comparemodels/comparemodels.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+image_file='../_images/dogs.jpg'
+number_of_choices=2
+
+choices=$(maskit list --all | awk '{print $1}' | sed '1d')
+
+echo "Select $number_of_choices models to compare:"
+select model in $choices; do
+    if [[ -n $model ]]; then
+        echo "You have selected $model"
+        selections+=("$model")
+        ((COUNT++))
+        if [[ $COUNT -eq $number_of_choices ]]; then
+            break
+        fi
+    else
+        echo "Invalid selection"
+    fi
+done
+
+output_files=()
+for model in "${selections[@]}"; do
+    echo "Running $model"
+    output_file=${model/:/-}.jpg
+    maskit run $model --image $image_file | base64 --decode > $output_file
+    echo "Exported $model.jpg"
+    output_files+=($output_file)
+done
+echo "Compare results: ${output_files[@]}"
