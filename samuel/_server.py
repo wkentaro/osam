766f6344901a51350eb008c062703210f593629d
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-01-26 20:06:48 +0900

    Implement /api/generate endpoint

--- samuel/_server.py
+++ samuel/_server.py
@@ -1,8 +1,49 @@
-import fastapi
+from typing import Optional
 
-app = fastapi.FastAPI()
+import fastapi
+import numpy as np
+import pydantic
+
+from samuel import _json
+from samuel import _models
+from samuel import _types
+
+app: fastapi.FastAPI = fastapi.FastAPI()
+
+model: Optional[_models.ModelBase] = None
 
 
 @app.get("/")
 def index():
     return fastapi.Response("Samuel is running")
+
+
+class GenerateRequest(pydantic.BaseModel):
+    model: str
+    image: str
+    prompt: _types.Prompt
+
+
+class GenerateResponse(pydantic.BaseModel):
+    mask: str
+
+
+@app.post("/api/generate")
+def generate(request: GenerateRequest) -> GenerateResponse:
+    global model
+
+    if model is None or model.name != request.model:
+        for model_cls in _models.MODELS:
+            if model_cls.name == request.model:
+                model = model_cls()
+                break
+        else:
+            raise fastapi.HTTPException(404, "Model not found")
+
+    image: np.ndarray = _json.image_b64data_to_ndarray(request.image)
+    image_embedding: _types.ImageEmbedding = model.encode_image(image=image)
+    mask: np.ndarray = model.generate_mask(
+        image_embedding=image_embedding, prompt=request.prompt
+    )
+
+    return GenerateResponse(mask=_json.image_ndarray_to_b64data(mask))
--- samuel/_types.py
+++ samuel/_types.py
@@ -1,6 +1,9 @@
 import dataclasses
+from typing import List
 
 import numpy as np
+import pydantic
+from typing_extensions import Annotated
 
 
 @dataclasses.dataclass
@@ -10,7 +13,42 @@ class ImageEmbedding:
     embedding: np.ndarray
 
 
-@dataclasses.dataclass
-class Prompt:
-    points: np.ndarray
-    point_labels: np.ndarray
+class Prompt(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(arbitrary_types_allowed=True)
+
+    points: Annotated[
+        np.ndarray,
+        pydantic.BeforeValidator(lambda x: np.asarray(x, dtype=np.float32)),
+        pydantic.PlainSerializer(lambda x: x.tolist()),
+    ]
+    point_labels: Annotated[
+        np.ndarray,
+        pydantic.BeforeValidator(lambda x: np.asarray(x, dtype=np.int32)),
+        pydantic.PlainSerializer(lambda x: x.tolist()),
+    ]
+
+    @pydantic.validator("points")
+    def validate_points(cls, points):
+        if points.ndim != 2:
+            raise ValueError("points must be 2-dimensional")
+        if points.shape[1] != 2:
+            raise ValueError("points must have 2 columns")
+        return points
+
+    @pydantic.field_serializer("points")
+    def serialize_points(cls, points: np.ndarray) -> List[List[float]]:
+        return points.tolist()
+
+    @pydantic.validator("point_labels")
+    def validate_point_labels(cls, point_labels, values):
+        if point_labels.ndim != 1:
+            raise ValueError("point_labels must be 1-dimensional")
+        if "points" in values and point_labels.shape[0] != values["points"].shape[0]:
+            raise ValueError("point_labels must have the same number of rows as points")
+        if not set(np.unique(point_labels).tolist()).issubset({0, 1}):
+            raise ValueError("point_labels must contain only 0s and 1s")
+        return point_labels
+
+    @pydantic.field_serializer("point_labels")
+    def serialize_point_labels(cls, point_labels: np.ndarray) -> List[int]:
+        return point_labels.tolist()
