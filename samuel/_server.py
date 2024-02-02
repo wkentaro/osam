bc9f84c982fe5ce2ffb0a28a498d39d15ba7010e
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-02-02 21:57:21 +0900

    Add logging for uvicorn access logs

--- samuel/_server.py
+++ samuel/_server.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 import fastapi
 
 from samuel import apis
@@ -7,13 +10,25 @@ app: fastapi.FastAPI = fastapi.FastAPI()
 
 
 @app.get("/")
-def index():
+async def index():
     return fastapi.Response("Samuel is running")
 
 
 @app.post("/api/generate_mask")
-def generate(request: types.GenerateMaskRequest) -> types.GenerateMaskResponse:
+async def generate(request: types.GenerateMaskRequest) -> types.GenerateMaskResponse:
     try:
         return apis.generate_mask(request=request)
     except ValueError as e:
         raise fastapi.HTTPException(400, str(e))
+
+
+# https://github.com/tiangolo/fastapi/issues/1508#issuecomment-638365277
+@app.on_event("startup")
+async def startup_event():
+    uvicorn_logger = logging.getLogger("uvicorn")
+    os.makedirs(os.path.expanduser("~/.cache/samuel/logs"), exist_ok=True)
+    handler = logging.FileHandler(os.path.expanduser("~/.cache/samuel/samuel.log"))
+    handler.setLevel(logging.DEBUG)
+    fmt = "%(asctime)s [%(levelname)s] %(module)s:%(funcName)s:%(lineno)s - %(message)s"
+    handler.setFormatter(logging.Formatter(fmt=fmt))
+    uvicorn_logger.addHandler(handler)
