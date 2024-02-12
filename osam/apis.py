2dfc795e04683eee7dbfe85943a23083a0503186
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-02-12 22:27:38 +0900

    Avoid inplace modification of the request object in the generate function

--- osam/apis.py
+++ osam/apis.py
@@ -24,17 +24,19 @@ def generate(request: types.GenerateRequest) -> types.GenerateResponse:
 
     if request.prompt is None:
         height, width = image.shape[:2]
-        request.prompt = types.Prompt(
+        prompt = types.Prompt(
             points=np.array([[width / 2, height / 2]], dtype=np.float32),
             point_labels=np.array([1], dtype=np.int32),
         )
         logger.warning(
             "Prompt is not given, so using the center point as prompt: {prompt!r}",
-            prompt=request.prompt,
+            prompt=prompt,
         )
+    else:
+        prompt = request.prompt
 
     image_embedding: types.ImageEmbedding = model.encode_image(image=image)
     mask: np.ndarray = model.generate_mask(
-        image_embedding=image_embedding, prompt=request.prompt
+        image_embedding=image_embedding, prompt=prompt
     )
     return types.GenerateResponse(model=request.model, mask=mask)
