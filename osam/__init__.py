adaf2f7bf8daac406e0245580e13ef86688d67b1
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-06-22 18:43:25 +0900

    Use osam_core in osam

--- osam/__init__.py
+++ osam/__init__.py
@@ -1,3 +1,7 @@
 import importlib.metadata
 
 __version__ = importlib.metadata.version("osam")
+
+from . import _models  # noqa: F401
+from . import apis  # noqa: F401
+from . import types  # noqa: F401
