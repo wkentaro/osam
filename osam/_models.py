17e0b17ca0afca36666864e9b737ecb2d73cdc60
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-06-22 20:14:51 +0900

    Use osam-sam and osam-efficientsam

---
+++ osam/_models.py
@@ -0,0 +1,3 @@
+# import modules to register the models
+import osam_sam  # noqa: F401
+import osam_efficientsam  # noqa: F401
--- osam/_models/__init__.py
+++
@@ -1,2 +0,0 @@
-from osam._models import _efficient_sam  # noqa: F401
-from osam._models import _sam  # noqa: F401
