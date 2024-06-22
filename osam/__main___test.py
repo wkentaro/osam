4f546f9e8fe47a0206a24815d64e0ef002da267c
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-06-22 20:25:43 +0900

    Fix __main___test.py

--- osam/__main___test.py
+++ osam/__main___test.py
@@ -9,7 +9,7 @@ here = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_run():
-    cmd = f"osam run efficient-sam:10m --image {here}/../examples/_images/dogs.jpg"
+    cmd = f"osam run efficientsam:10m --image {here}/../examples/_images/dogs.jpg"
     output = subprocess.check_output(shlex.split(cmd))
     image = PIL.Image.open(io.BytesIO(output))
     assert image.size == (2560, 1600)
