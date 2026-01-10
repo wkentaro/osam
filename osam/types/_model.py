2ec39bf49c25c310afa5324b8113f04ae7846f2d
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-01-10 12:11:41 +0900

    refactor(model): move inference session loading to helper functions

--- osam/types/_model.py
+++ osam/types/_model.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 import abc
 import hashlib
 from typing import Dict
 from typing import Optional
+from typing import Sequence
 
 import numpy as np
 import onnxruntime
@@ -21,43 +24,8 @@ class Model(abc.ABC):
 
     def __init__(self):
         logger.debug("Initializing model {name}", name=self.name)
-
         self.pull()
-
-        providers = None
-        self._inference_sessions = {}
-        for key, blob in self._blobs.items():
-            try:
-                # Try to use all of the available providers e.g., cuda, tensorrt.
-                if providers is None:
-                    if "CUDAExecutionProvider" in onnxruntime.get_available_providers():  # type: ignore[possibly-missing-attribute]
-                        providers = ["CUDAExecutionProvider", "CPUExecutionProvider"]
-                    else:
-                        providers = ["CPUExecutionProvider"]
-                inference_session = onnxruntime.InferenceSession(
-                    blob.path, providers=providers
-                )
-            except Exception as e:
-                # Even though there is fallback in onnxruntime, it won't always work.
-                # e.g., CUDA is installed and CUDA_PATH is set, but CUDA_VISIBLE_DEVICES
-                # is empty. We fallback to cpu in such cases.
-                logger.error(
-                    "Failed to create inference session with providers {providers!r}. "
-                    "Falling back to ['CPUExecutionProvider']: {e}",
-                    providers=providers,
-                    e=e,
-                )
-                providers = ["CPUExecutionProvider"]
-                inference_session = onnxruntime.InferenceSession(
-                    blob.path, providers=providers
-                )
-            self._inference_sessions[key] = inference_session
-
-            providers = inference_session.get_providers()
-        logger.info(
-            "Initialized inference sessions with providers {providers!r}",
-            providers=providers,
-        )
+        self._inference_sessions = _load_inference_sessions(blobs=self._blobs)
 
     @classmethod
     def pull(cls):
@@ -99,3 +67,48 @@ class Model(abc.ABC):
     @abc.abstractmethod
     def generate(self, request: GenerateRequest) -> GenerateResponse:
         pass
+
+
+def _load_inference_sessions(
+    blobs: dict[str, Blob],
+) -> dict[str, onnxruntime.InferenceSession]:
+    providers: Sequence[str] | None = None
+    inference_sessions: dict[str, onnxruntime.InferenceSession] = {}
+    for key, blob in blobs.items():
+        inference_session: onnxruntime.InferenceSession = _load_inference_session(
+            blob=blob, providers=providers
+        )
+        providers = inference_session.get_providers()
+
+        inference_sessions[key] = inference_session
+    logger.info(
+        "Initialized inference sessions with providers {providers!r}",
+        providers=providers,
+    )
+    return inference_sessions
+
+
+def _load_inference_session(
+    blob: Blob, providers: list[str] | None = None
+) -> onnxruntime.InferenceSession:
+    try:
+        # Try to use all of the available providers e.g., cuda, tensorrt.
+        if providers is None:
+            if "CUDAExecutionProvider" in onnxruntime.get_available_providers():  # type: ignore[possibly-missing-attribute]
+                providers = ["CUDAExecutionProvider", "CPUExecutionProvider"]
+            else:
+                providers = ["CPUExecutionProvider"]
+        inference_session = onnxruntime.InferenceSession(blob.path, providers=providers)
+    except Exception as e:
+        # Even though there is fallback in onnxruntime, it won't always work.
+        # e.g., CUDA is installed and CUDA_PATH is set, but CUDA_VISIBLE_DEVICES
+        # is empty. We fallback to cpu in such cases.
+        logger.error(
+            "Failed to create inference session with providers {providers!r}. "
+            "Falling back to ['CPUExecutionProvider']: {e}",
+            providers=providers,
+            e=e,
+        )
+        providers = ["CPUExecutionProvider"]
+        inference_session = onnxruntime.InferenceSession(blob.path, providers=providers)
+    return inference_session
